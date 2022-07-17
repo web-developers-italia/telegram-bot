37c3972a84096c3736b639dbcf42b06d2bc1e38b
Alexander Cerutti <cerutti.alexander@gmail.com>
2022-07-17 14:02:44 +0200

    Refactored rules and adapted text to MarkdownV2

--- functions/src/telegram/commands/sendRules.ts
+++ functions/src/telegram/commands/sendRules.ts
@@ -7,53 +7,35 @@ export function sendRules(
 ): Promise<Message> {
   return context.reply(
     `
-Regolamento:
-\`\`\`
-  - [✅] Richieste di supporto solo se specifiche, chiare, concise e accompagnate dalle soluzioni già provate
+*Regolamento*:
 
-    Va bene la pigrizia, ma il gruppo non è qui per imboccarvi la soluzione. Siate pro-attivi.
+  ✅ Richieste di supporto *solo se specifiche, chiare, concise e accompagnate dalle soluzioni già provate*\\. Il gruppo non è qui per imboccarti la soluzione\\. Siate pro\\-attivi\\.
 
-  - [✅] Offerte di lavoro **solo se accompagnate da tipo di contratto e range di retribuzione** (o budget)
+  ✅ Offerte di lavoro *solo se accompagnate da tipo di contratto e range di retribuzione* \\(o budget\\)\\. Pena la cancellazione del messaggio\\.
 
-    Il vostro messaggio verrà cancellato in caso contrario.
+  ✅ Discussioni su news, lavoro e lifestyle del web developer e affini\\.
 
-  - [✅] Discussioni su news, lavoro e lifestyle del web-developer e affini;
+  ❌ Richieste di aiuto in privato\\. il gruppo è qui per aiutare e per parlare\\. No, non intasi il flusso di chat con i tuoi problemi se chiedi qui\\. Scrivi /nonchiederedichiedere per il motivo\\.
 
-  - [❌] Richieste di aiuto in privato.
-  
-    Il gruppo è qui per aiutare e per parlare. No, non è vero che intaserai il flusso di chat con i tuoi problemi se chiedi dentro al gruppo.
-    Scrivere in privato significa chiedere a qualcuno di prendersi la responsabilità di risponderti e di risolvere il tuo problema.
+  ❌ Richieste di supporto per esercizi scolastici\\. Va bene richiedere risorse per risolvere autonomamente il problema\\.
 
-  - [❌] Richieste di supporto per esercizi assegnati da docenti o corsi scolastici.
+  ❌ Richieste di supporto non inerenti allo sviluppo web\\. Nessuno qui riparerà la tua stampante o creerà un plugin per il tuo server Minecraft\\.
 
-    E' possibile, ed è ben accetta, la possibilità di richiedere risorse da studiare per arrivare alla risoluzione del problema in maniera quasi autonoma.
+  ❌ Incollare codice direttamente in chat \\(tranne che per snippet brevi\\)\\. Utilizza servizi come Pastebin, Codepen o Stackblitz\\.
 
-  - [❌] Richieste di supporto non inerenti allo sviluppo web.
+  ❌ Spam di qualsiasi forma nel flusso di chat\\.
 
-    Nessuno qui riparerà la tua stampante o ti aiuterà a creare il plugin per il tuo server Minecraft
+  ❌ Foto agli schermi\\. Sono accettati screenshot Impara ad usare gli strumenti della tua macchina\\.
 
-  - [❌] Incollare codice direttamente in chat (si fanno eccezioni per snippet brevi).
+  ❌ Gore, porno, nudità e tutto ciò che può urtare la sensibilità dei membri del gruppo\\. Incluse le foto profilo\\. Nel gruppo sono presenti membri minorenni\\.
 
-    Si richiede di utilizzare servizi come Pastebin, Codepen o Stackblitz in modo da aiutare chi decide di aiutare.
+  ❌ Mandare messaggi con canali invece del proprio profilo personale\\.
 
-  - [❌] Spam di qualsiasi forma nel flusso di chat
+Gli utenti sono tenuti a evitare comportamenti adeguati al fine di mantenere stabile e non tossica la situazione nella chat\\.
+Gli amministratori possono bandire qualunque membro dalla chat qualora possa venir identificato come potenziale problema\\. Nessuno deve spiegazioni a nessuno\\.
 
-  - [❌] Foto agli schermi. Sono accettati screenshot.
-  
-    Imparate ad usare gli strumenti della vostra macchina.
-
-  - [❌] Gore, porno, nudità e tutto ciò che può urtare la sensibilità dei membri del gruppo.
-  
-    Sono incluse le foto profilo. Nel gruppo sono presenti membri minorenni.
-
-  - [❌] Mandare messaggi con canali invece del proprio profilo personale
-  
-Gli utenti sono tenuti a evitare comportamenti adeguati al fine di mantenere stabile la situazione nella chat.
-Gli amministratori possono bandire qualunque membro dalla chat qualora possa venir identificato come potenziale problema. Nessuno deve spiegazioni a nessuno.
-
-\`\`\`
-
-  Contribuisci al gruppo: https://github.com/insieme-dev/community`,
+[Contribuisci al gruppo su Github](https://github\\.com/insieme\\-dev/community)
+`,
     {
       reply_to_message_id: replyTo || undefined,
       parse_mode: "MarkdownV2",
