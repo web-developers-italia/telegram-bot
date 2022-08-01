61636a1582cc12ae7de1676e1d91c26e4983f65a
Marco 'Gatto' Boffo <chat@marcoboffo.me>
2022-08-01 06:29:03 +0200

    fix: typo

--- functions/src/telegram/commands/contribute.ts
+++ functions/src/telegram/commands/contribute.ts
@@ -4,9 +4,9 @@ import type { Message } from "telegraf/typings/core/types/typegram";
 export function contribute(context: Context): Promise<Message> {
   return context.reply(
     `
-*Tramite il repository open source, puoi amministrare il gruppo democraticamente, decidere le regoli, gli amministratori e il futuro del gruppo\\.*
+*Tramite il repository open source, puoi amministrare il gruppo democraticamente, decidere le regole, gli amministratori e il futuro del gruppo\\.*
 
-Contribuisci al gruppo: https://github\\.com/insieme\\-dev/community
+✍️ https://github\\.com/insieme\\-dev/community
 `,
     {
       parse_mode: "MarkdownV2",
