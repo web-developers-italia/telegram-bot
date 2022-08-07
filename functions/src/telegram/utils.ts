230823c6bccd576deccdb848175ad6fac85c9973
ImVector <59611597+LuigiImVector@users.noreply.github.com>
2022-08-07 17:58:15 +0200

    feat: /contribute shows a list of open PRs and Issues

--- functions/src/telegram/utils.ts
+++ functions/src/telegram/utils.ts
@@ -1,4 +1,25 @@
+import { fetch } from 'undici'
+
 export const addUsernameCommand = (command: string) => [
   command,
   `${command}@${process.env.TELEGRAM_BOT_USERNAME}`,
 ];
+
+export const getRequest = async (url: string) => {
+  const res = await fetch(url)
+  const json = await res.json() 
+
+  return JSON.stringify(json);
+};
+
+export const getListGithub = async (url: string) => {
+  const json = JSON.parse(await getRequest(url));
+  let list: string[] = [];
+  for(let key in json) {
+    let link: string = json[key].html_url;
+    let title: string = json[key].title;
+    list.push(`[${title}](${link})`.replaceAll('.', '\\.').replaceAll('-', '\\-'));
+ }
+
+ return list;
+};
