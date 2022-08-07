3af86b109badb564ecae2f8822582e8586831b91
ImVector <59611597+LuigiImVector@users.noreply.github.com>
2022-08-07 17:58:15 +0200

    feat: /contribute shows a list of open PRs and Issues

--- functions/src/telegram/commands/contribute.ts
+++ functions/src/telegram/commands/contribute.ts
@@ -1,12 +1,19 @@
 import type { Context } from "telegraf";
 import type { Message } from "telegraf/typings/core/types/typegram";
+import { getListGithub } from "../utils";
 
-export function contribute(context: Context): Promise<Message> {
+export async function contribute(context: Context): Promise<Message> {
   return context.reply(
     `
 *Tramite il repository open source, puoi amministrare il gruppo democraticamente, decidere le regole, gli amministratori e il futuro del gruppo\\.*
 
 ✍️ https://github\\.com/${process.env.REPOSITORY_NAME?.replaceAll('-', '\\-')}
+
+Pull Request attive:
+${(await getListGithub(`https://api.github.com/repos/${process.env.REPOSITORY_NAME}/pulls?state=open`)).join('\n')}
+
+Issue attive:
+${(await getListGithub(`https://api.github.com/repos/${process.env.REPOSITORY_NAME}/issues?state=open`)).join('\n')}
 `,
     {
       parse_mode: "MarkdownV2",
