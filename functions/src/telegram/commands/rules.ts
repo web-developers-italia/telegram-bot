1dd14ca82fbfe264a97c003dbcff6094674d00c7
Marco 'Gatto' Boffo <chat@marcoboffo.me>
2022-08-08 09:34:15 +0200

    feat: clearer rules

--- functions/src/telegram/commands/rules.ts
+++ functions/src/telegram/commands/rules.ts
@@ -12,25 +12,25 @@ export function rules(
 
 ✅ Richieste di supporto *solo se specifiche, chiare, concise e accompagnate dalle soluzioni già provate*\\.
 
-✅ Offerte di lavoro *solo se accompagnate da tipo di contratto e range di retribuzione* \\(o budget\\)\\.
-
-✅ Discussioni su news, lavoro e lifestyle del web developer e affini\\.
-
-✅ Richieste di supporto inerenti allo sviluppo web professionale\\.
-
-✅ Codice condiviso tramite strumenti specifici (Pastebin, Codepen, Stackblitz) e screenshot nitidi\\.
-
-❌ Abuso di sticker e messaggi vocali\\.
-
 ❌ Richieste di aiuto in privato\\.
 
-✅ Richieste di supporto per riuscire a risolvere autonomamente esercizi scolastici\\.
+✅ Offerte di lavoro *solo se accompagnate da tipo di contratto e range di retribuzione* \\(o budget\\)\\.
 
 ❌ Spam di qualsiasi forma nel flusso di chat\\.
 
+✅ Discussioni su news, lavoro e lifestyle del web developer e affini\\.
+
 ❌ Gore, porno, nudità e tutto ciò che può urtare la sensibilità dei membri del gruppo\\. Valido anche per le foto profilo\\.
 
+✅ Richieste di supporto inerenti allo sviluppo web professionale\\.
+
+❌ Abuso di sticker e messaggi vocali\\.
+
+✅ Codice condiviso tramite strumenti specifici (Pastebin, Codepen, Stackblitz) e screenshot nitidi\\.
+
 ❌ Mandare messaggi con canali invece del proprio profilo personale\\.
+
+✅ Richieste di supporto per riuscire a risolvere autonomamente esercizi scolastici\\.
 
 Gli utenti sono tenuti a evitare comportamenti socialmente inadeguati, al fine di mantenere stabile e positiva la comunicazione nella chat\\.
 
