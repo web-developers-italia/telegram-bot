cf00639bc849af52f43ac2c0ab0f7678d85b451d
Marco 'Gatto' Boffo <chat@marcoboffo.me>
2022-08-08 13:04:55 +0200

    fix: no escape on nick lost

--- functions/src/telegram/commands/admin.ts
+++ functions/src/telegram/commands/admin.ts
@@ -1,13 +1,12 @@
 import type { Context } from "telegraf";
 import type { Message } from "telegraf/typings/core/types/typegram";
-import { escapeForTelegram } from "../utils";
 
 export async function admin(context: Context): Promise<Message> {
   const admins = await context.getChatAdministrators();
 
   const nicks = admins.map((a) => `@${a.user.username}`).join(" ");
 
-  return context.reply(escapeForTelegram(nicks), {
+  return context.reply(nicks, {
     parse_mode: "MarkdownV2",
     reply_to_message_id: context.message
       ? context.message.message_id
