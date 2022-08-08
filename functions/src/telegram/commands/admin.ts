3f2cd4feb2059500fede93aac857e064e7601ce2
Alexander Cerutti <cerutti.alexander@gmail.com>
2022-08-08 18:42:13 +0200

    Changed admin tagging to invisible characters

--- functions/src/telegram/commands/admin.ts
+++ functions/src/telegram/commands/admin.ts
@@ -1,12 +1,23 @@
 import type { Context } from "telegraf";
 import type { Message } from "telegraf/typings/core/types/typegram";
+import { escapeForTelegram } from "../utils";
 
 export async function admin(context: Context): Promise<Message> {
   const admins = await context.getChatAdministrators();
 
-  const nicks = admins.map((a) => `@${a.user.username}`).join(" ");
+  const tags = admins.reduce<string[]>((acc, current) => {
+	if (current.user.is_bot) {
+		return acc;
+	}
 
-  return context.reply(nicks, {
+	return [
+		...acc,
+		// Invisible character on purpose
+		`[ ](tg://user?id=${current.user.id})`
+	];
+  }, []);
+
+  return context.reply(escapeForTelegram(`Gli amministratori sono stati notificati. ${tags.join("")}`), {
     parse_mode: "MarkdownV2",
     reply_to_message_id: context.message
       ? context.message.message_id
