ef50261e024679da8acebd1b2ee9142bfa65e8f3
Alexander Cerutti <cerutti.alexander@gmail.com>
2022-08-08 20:52:43 +0200

    Fixed typechecking due to wrong merge conflict resolution

--- functions/src/telegram/commands/pong.ts
+++ functions/src/telegram/commands/pong.ts
@@ -3,12 +3,12 @@ import type { Message } from "telegraf/typings/core/types/typegram";
 import type { CommandsProtocol } from "../CommandsProtocol";
 import { escapeForTelegram } from "../utils";
 
-export function pong(context: Context): Promise<Message> {
+export const pong: CommandsProtocol<Message> = function (context: Context) {
 	return context.reply(escapeForTelegram(`/pong 🏓`), {
 		parse_mode: "MarkdownV2",
 		disable_web_page_preview: true,
 		reply_to_message_id: context.message?.message_id ?? undefined,
 	});
-}
+};
 
 pong.triggers = ["/ping"];
