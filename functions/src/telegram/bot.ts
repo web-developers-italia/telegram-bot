32b17aa31d445c0b59ba30872728a9f6d769ee72
Alexander Cerutti <cerutti.alexander@gmail.com>
2022-08-08 14:27:44 +0200

    Prettier initial format

--- functions/src/telegram/bot.ts
+++ functions/src/telegram/bot.ts
@@ -11,32 +11,32 @@ import { pong } from "./commands/pong";
 import { addUsernameCommand } from "./utils";
 
 declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      TELEGRAM_BOT_KEY: string;
-    }
-  }
+	namespace NodeJS {
+		interface ProcessEnv {
+			TELEGRAM_BOT_KEY: string;
+		}
+	}
 }
 
 const telegramBot: Telegraf<Context> = new Telegraf(
-  process.env.TELEGRAM_BOT_KEY
+	process.env.TELEGRAM_BOT_KEY,
 );
 
 telegramBot.hears(["@admin", "/admin"].map(addUsernameCommand).flat(), admin);
 
 telegramBot.hears(
-  ["/regolamento", "/regole", "/rules"].map(addUsernameCommand).flat(),
-  rules
+	["/regolamento", "/regole", "/rules"].map(addUsernameCommand).flat(),
+	rules,
 );
 
 telegramBot.hears(
-  ["/contribute", "/contribuisci"].map(addUsernameCommand).flat(),
-  contribute
+	["/contribute", "/contribuisci"].map(addUsernameCommand).flat(),
+	contribute,
 );
 
 telegramBot.hears(
-  ["/dontasktoask", "/nonchiederedichiedere"].map(addUsernameCommand).flat(),
-  dontasktoask
+	["/dontasktoask", "/nonchiederedichiedere"].map(addUsernameCommand).flat(),
+	dontasktoask,
 );
 
 telegramBot.hears(["/rielabora"].map(addUsernameCommand).flat(), rielabora);
@@ -48,8 +48,8 @@ telegramBot.hears(["/ping"].map(addUsernameCommand).flat(), pong);
 telegramBot.on("message", setLastMemberActivity);
 
 export const bot = functions
-  .region("europe-west1")
-  .https.onRequest((req, res) => {
-    telegramBot.handleUpdate(req.body, res);
-    res.sendStatus(200);
-  });
+	.region("europe-west1")
+	.https.onRequest((req, res) => {
+		telegramBot.handleUpdate(req.body, res);
+		res.sendStatus(200);
+	});
--- functions/src/telegram/commands/admin.ts
+++ functions/src/telegram/commands/admin.ts
@@ -2,14 +2,14 @@ import type { Context } from "telegraf";
 import type { Message } from "telegraf/typings/core/types/typegram";
 
 export async function admin(context: Context): Promise<Message> {
-  const admins = await context.getChatAdministrators();
+	const admins = await context.getChatAdministrators();
 
-  const nicks = admins.map((a) => `@${a.user.username}`).join(" ");
+	const nicks = admins.map((a) => `@${a.user.username}`).join(" ");
 
-  return context.reply(nicks, {
-    parse_mode: "MarkdownV2",
-    reply_to_message_id: context.message
-      ? context.message.message_id
-      : undefined,
-  });
+	return context.reply(nicks, {
+		parse_mode: "MarkdownV2",
+		reply_to_message_id: context.message
+			? context.message.message_id
+			: undefined,
+	});
 }
